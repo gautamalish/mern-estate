55a2a2adc80fdf1901c4d6729ad25765d36b95a1
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-08-07 06:05:55 +0545

    Header created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
